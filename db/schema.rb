7cd39fa73db835055409f213a9d562fd0eb8691d
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-05-25 14:54:47 +0000

    finish 4-2-2

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210522095932) do
+ActiveRecord::Schema.define(version: 2021_05_25_143432) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "name", null: false
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
