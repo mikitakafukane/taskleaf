d838a976d41f411db119bb488161747867d6401c
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-08 08:56:14 +0000

    finish7-5

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   config.logger.formatter = proc {|severity, timestamp, progname, message|
     "#{timestamp}: #{severity}: #{message}\n"
   }
+  
+  # Store uploaded files on the local file system (see config/storage.yml for options)
+  config.active_storage.service = :local
+
 end
