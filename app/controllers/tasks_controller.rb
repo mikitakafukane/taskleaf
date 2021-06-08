74a8945fa8989258a81e9d7dfa3cb5252e25af41
mikitakafukane <arsene.miyaniii.4@gmail.com>
2021-06-08 08:08:04 +0000

    finish 7-4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,7 @@ class TasksController < ApplicationController
     
     if @task.save
       # logger.debug "task: #{@task.attributes.inspect}"
+      TaskMailer.creation_email(@task).deliver_now
       redirect_to tasks_url, notice: "タスク「#{@task.name}」を登録しました。"
     else
       render :new
